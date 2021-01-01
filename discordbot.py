4ae12be1382259f3d0f5735c93294d4f618460a5
WOZNYDEV <75819438+WOZNYDEV@users.noreply.github.com>
2021-01-02 02:31:33 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -30,7 +30,7 @@ def countKills():
 async def on_command_error(ctx, error):
     orig_error = getattr(error, "original", error)
     error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+    # await ctx.send(error_msg)
 
 
 @bot.command()
